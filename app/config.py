f12a3f4e525d5ac5cff51ae0449d9021bd58328a
Zibing Zhang <zibing.zha@gmail.com>
2021-08-30 12:43:10 -0400

    Group definitions are local

--- app/config.py
+++ app/config.py
@@ -1,15 +1,13 @@
 from typing import List
-from app.groups import (
-    GroupDefinition,
-    group_condition,
-)
-from app.local.group_conditions import *
+import typing
+from app.local import group_definitions
+
+if typing.TYPE_CHECKING:
+    from app.groups import GroupDefinition
 
 # Login information
 USERNAME: str = "username"
 PASSWORD: str = "password"
 
 # Group definitions
-GROUP_DEFINITIONS: List[GroupDefinition] = [
-    GroupDefinition("Group Name", group_condition),
-]
+GROUP_DEFINITIONS: List["GroupDefinition"] = group_definitions.GROUP_DEFINITIONS
---
+++ app/local/group_definitions.py
@@ -0,0 +1,5 @@
+from typing import List
+from app.groups import GroupDefinition
+
+
+GROUP_DEFINITIONS: List[GroupDefinition] = []
